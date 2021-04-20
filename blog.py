3a82cc3b3f9462fcdb351b3cf6df665473c1bb7d
Javier Rozas <javier@lotus23.tech>
2021-04-19 22:36:54 -0400

    🎉 Trabajo estructural

--- blog.py
+++ blog.py
@@ -1,6 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+isUser = False
+
+posts = [
+    {
+        'autor':'Javier Rozas',
+        'titulo':'Post N°1 del Blog',
+        'contenido':'Condenido del primer post',
+        'fecha':'19 Abril 2021'
+    },
+    {
+        'autor':'Danae Gonzalez',
+        'titulo':'Post N°2 del Blog',
+        'contenido':'Condenido del segundo post',
+        'fecha':'21 Abril 2021'
+    },    
+    ]
 
 @app.route('/')
 def blog():
-    return render_template('index.html')
+    return render_template('index.html', posts=posts, isUser=False)
+
+@app.route('/post')
+def post():
+    return render_template('post.html')    
