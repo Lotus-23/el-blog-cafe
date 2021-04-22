d8896dd2924d9bb13846246cb83211a40a0bcb64
Javier Rozas <javier@lotus23.tech>
2021-04-22 08:22:38 -0400

    🚧 progreso formulario de registro y login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import FormularioRegistro, FormularioLogin
 app = Flask(__name__)
 
@@ -32,11 +32,22 @@ def post():
 @app.route('/registro', methods=['GET', 'POST'])
 def registro():
     form = FormularioRegistro()
+    if form.validate_on_submit():
+        flash(f'Registro satisfactorio para el usuario { form.usuario.data }!', 'success')
+        return redirect(url_for('blog'))
+
     return render_template('registro.html', titulo='Registro', form=form)      
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = FormularioLogin()
+    if form.validate_on_submit():
+        if form.correo.data == 'admin@blog.com' and form.password.data == 'password':
+            flash(f'Ingresaste al Blog Café !', 'success')
+            return redirect(url_for('blog'))
+        else:
+            flash(f'El ingreso falló, por favor revise su nombre de usuario y contraseña', 'danger')
+
     return render_template('login.html', titulo='Log In', form=form)      
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,21 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, Email, InputRequired
+from wtforms.fields.html5 import EmailField
 
 class FormularioRegistro(FlaskForm):
 
-    usuario = StringField('Nombre de Usuario', validators=[DataRequired(), Length(min=3, max=20)])
-    #correo = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Contraseña', validators=[DataRequired()])
-    confirma_password = PasswordField('Confirme su Contraseña', validators=[DataRequired(), EqualTo('password')])
+    usuario = StringField('Nombre de Usuario', validators=[InputRequired("Por favor ingrese su nombre de usuario"), Length(min=3, max=20)])
+    correo = EmailField('Email', validators=[InputRequired("Por favor ingrese su correo electrónico"), Email()])
+    password = PasswordField('Contraseña', validators=[InputRequired("Por favor ingrese su contraseña")])
+    confirma_password = PasswordField('Confirme su Contraseña', validators=[InputRequired("Por favor vuelva a ingresasr su contraseña"), EqualTo('password')])
 
     enviar = SubmitField('Regístrate!')
 
 
 class FormularioLogin(FlaskForm):
 
-    #correo = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Contraseña', validators=[DataRequired()])
+    correo = EmailField('Email', validators=[InputRequired("Por favor ingrese su correo electrónico"), Email()])
+    password = PasswordField('Contraseña', validators=[InputRequired("Por favor ingrese su contraseña") ])
     recuerdame = BooleanField('Recuérdame')
 
     enviar = SubmitField('Ingresar')    
